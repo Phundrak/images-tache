d533d59875d82413c59afe94a68a91de41160ba0
Kofil <kat.kofil@yahoo.fr>
2017-12-03 21:31:53 +0100

    pas encore test

--- src/black_border.c
+++ src/black_border.c
@@ -1,3 +1,59 @@
-void black_border(struct Image *img, struct Zone ){
+#include "utilities.h"
 
+bool pix_is_border(struct Image *img, struct Pixel *pix, struct Zone *z){
+	Pixel *p;
+	if ((*pix).x < (*img).sizeX + 1){
+		p = pix_at_img(img , (*pix).x + 1, (*pix).y);
+		if (strcomp(&p->zone->R, &z->R) == true)
+			return true;
+		if (strcomp(&p->zone->R, &z->R) == true)
+			return true;
+		if (strcomp(&p->zone->R, &z->R) == true)
+			return true;
+	}
+	if ((*pix).x > 0){
+		p = pix_at_img(img , pix->x - 1, pix->y);
+		if (strcomp(&p->zone->R, &z->R) == true)
+			return true;
+		if (strcomp(&p->zone->R, &z->R) == true)
+			return true;
+		if (strcomp(&p->zone->R, &z->R) == true)
+			return true;
+	}
+	if ((*pix).y < (*img).sizeY + 1){
+		p = pix_at_img(img , pix->x, pix->y + 1);
+		if (strcomp(&p->zone->R, &z->R) == true)
+			return true;
+		if (strcomp(&p->zone->R, &z->R) == true)
+			return true;
+		if (strcomp(&p->zone->R, &z->R) == true)
+			return true;
+	}
+	if ((*pix).y > 0){
+		p = pix_at_img(img , pix->x, pix->y - 1);
+		if (strcomp(&p->zone->R, &z->R) == true)
+			return true;
+		if (strcomp(&p->zone->R, &z->R) == true)
+			return true;
+		if (strcomp(&p->zone->R, &z->R) == true)
+			return true;
+	}
+	return false;
+}
+
+void put_pix_color(struct Pixel *pix, char r, char g, char b){
+	(*pix->R) = r;
+	(*pix->G) = g;
+	(*pix->B) = b;
+}
+
+void black_border(struct Image *img, struct Zone *z){
+	int y, x;
+	
+	for (y = 0; y < (*img).sizeY; y++){
+		for (x = 0; x < (*img).sizeX; x++){
+			if (pix_is_border(img, pix_at_img(img, x, y), z) == true)
+				put_pix_color(pix_at_img(img, x, y), 0, 0, 0);
+		}
+	}
 }
--- src/process.c
+++ src/process.c
@@ -20,9 +20,9 @@ bool pix_already_in_area(struct Pixel *pix, struct Zone *zt){
 
 bool pix_to_add(struct Pixel *pix, struct Zone *zt){
 	int diff_R, diff_G, diff_B;
-	diff_R = ((abs((int)zt->R - (int)pix->R)) * 100 / 255);
-	diff_G = ((abs((int)zt->G - (int)pix->G)) * 100 / 255);
-	diff_B = ((abs((int)zt->B - (int)pix->B)) * 100 / 255);
+	diff_R = ((abs((int)(*zt).R - (int)(*pix).R)) * 100 / 255);
+	diff_G = ((abs((int)(*zt).G - (int)(*pix).G)) * 100 / 255);
+	diff_B = ((abs((int)(*zt).B - (int)(*pix).B)) * 100 / 255);
 	return (((diff_R + diff_G + diff_B) / 3) <= TOLERANCE);
 }
 
@@ -34,22 +34,22 @@ void running_area(struct Pixel *pix, struct Image *img, struct Zone *zt){
 	//test si le pixel appartien deja a une zone
 	if (test_exist(pix) == true){
 		//test si le pixel de droite est dans les borne
-		if ((pix->x + 1) < img->sizeX)
-			running_area(img_at(img, pix->x + 1, pix->y), img, zt);
+		if ((*pix).x + 1 < (*img).sizeX)
+			running_area(pix_at_img(img, pix->x + 1, pix->y), img, zt);
 		//test si le pixel du haut est dans les borne
-		if ((pix->y + 1) < img->sizeY)
-			running_area(img_at(img, pix->x, pix->y + 1), img, zt);
+		if ((*pix).y + 1 < (*img).sizeY)
+			running_area(pix_at_img(img, pix->x, pix->y + 1), img, zt);
 		//test si le pixel de gauche est dans les borne
-		if ((pix->x - 1) > 0)
-			running_area(img_at(img, pix->x - 1, pix->y), img, zt);
+		if ((*pix).x - 1 > 0)
+			running_area(pix_at_img(img, pix->x - 1, pix->y), img, zt);
 		//test si le pixel du bas est dans les borne
-		if ((pix->y - 1) > 0)
-			running_area(img_at(img, pix->x, pix->y - 1), img, zt);
+		if ((*pix).y - 1 > 0)
+			running_area(pix_at_img(img, pix->x, pix->y - 1), img, zt);
 	}
 	if (pix_to_add(pix, zt) == true){
 		//ajoute le pixel a la liste des pixels de la zone et attribut la zone au Pixel
 		zone_add_pix(zt, pix);
-		runnig_area(pix, img, zt);
+		running_area(pix, img, zt);
 	}
 }
 
@@ -59,17 +59,18 @@ void map_area(struct Pixel *pix, struct Image *img){
 	struct Zone *zt;
 
 	zt = new_zone(img,pix);
+	img_add_zone(img, zt);
 	running_area(pix, img, zt);
 }
 
 //la fonction parcour pixel par pixel l image
 void process(struct Image *img){
 	unsigned long x, y;
-for(y = 0; y < img->sizeY; y++){
-		for(x = 0; x < img->sizeX; x++){
+	for(y = 0; y < (*img).sizeY; y++){
+		for(x = 0; x < (*img).sizeX; x++){
 			//check si le pixel actuel possede deja une Zone
-			if (test_exist(img_at(img,x,y)) == false)
-				map_area(img_at(img, x, y), img);
+			if (test_exist(pix_at_img(img,x,y)) == false)
+				map_area(pix_at_img(img, x, y), img);
 		}
 	}
 }
