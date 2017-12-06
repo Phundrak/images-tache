d3fee30dd2301dda8f7f0acd5a9a239ddd2ac66a
Phuntsok Drak-pa <luccar25@gmail.com>
2017-12-06 03:47:41 +0100

    added algo functions to header

--- src/algorithm.c
+++ src/algorithm.c
@@ -28,26 +28,28 @@ void generate_segment(Zone_t zone, Pixel_t pix) {
   segment->xd = xd;
   zone_add_segment(zone, segment);
 
+  PDEB("iter: %lu\nnb zones: %lu\nnb segments: %lu\n", iter++, image->nb_zones,
+       zone->nb_segment);
 
-  PDEB("iter: %lu\nnb zones: %lu\nnb segments: %lu\n", iter++, image->nb_zones, zone->nb_segment);
-
-  for(i = xg; i < xd; i++) {
+  for (i = xg; i < xd; i++) {
     pixel = pix_at_img(i, pix->y);
     pixel->processed = true;
   }
 
   /* pour chaque pixel supérieur aux pixels du segment */
-  for(i = xg; i < xd; i++) {
+  for (i = xg; i < xd; i++) {
     pixel = pix_at_img(i, pix->y + 1);
-    /* si le pixel existe, qu'il est non-traité et qu'il est de la bonne couleur */
-    if(pixel && !pixel->processed && tolerated(pixel, zone))
+    /* si le pixel existe, qu'il est non-traité et qu'il est de la bonne couleur
+     */
+    if (pixel && !pixel->processed && tolerated(pixel, zone))
       generate_segment(zone, pixel);
   }
   /* pour chaque pixel inférieur aux pixels du segment */
-  for(i = xg; i < xd; i++) {
+  for (i = xg; i < xd; i++) {
     pixel = pix_at_img(i, pix->y - 1);
-    /* si le pixel existe, qu'il est non-traité et qu'il est de la bonne couleur */
-    if(pixel && !pixel->processed && tolerated(pixel, zone))
+    /* si le pixel existe, qu'il est non-traité et qu'il est de la bonne couleur
+     */
+    if (pixel && !pixel->processed && tolerated(pixel, zone))
       generate_segment(zone, pixel);
   }
 }
@@ -63,21 +65,22 @@ void make_zones() {
   iter = 0;
   unsigned long i, size;
   size = image->sizeX * image->sizeY;
-  for(i = 0; i < size; i++) /* pour chaque pixel */
-    if(!image->pixels[i]->processed) /* si le pixel n'a pas été traité */
+  for (i = 0; i < size; i++)          /* pour chaque pixel */
+    if (!image->pixels[i]->processed) /* si le pixel n'a pas été traité */
       generate_zone(image->pixels[i]);
 }
 
-void change_color(Pixel_t pix, unsigned char R, unsigned char G, unsigned char B) {
+void change_color(Pixel_t pix, unsigned char R, unsigned char G,
+                  unsigned char B) {
   *pix->R = R;
   *pix->G = G;
   *pix->B = B;
 }
 
 void invert_color(Pixel_t pix) {
-  *pix->R = (unsigned char) (255 - *pix->R);
-  *pix->G = (unsigned char) (255 - *pix->G);
-  *pix->B = (unsigned char) (255 - *pix->B);
+  *pix->R = (unsigned char)(255 - *pix->R);
+  *pix->G = (unsigned char)(255 - *pix->G);
+  *pix->B = (unsigned char)(255 - *pix->B);
 }
 
 void black_border() {
@@ -85,8 +88,8 @@ void black_border() {
   Pixel_t pix1, pix2;
   Segment_t seg;
 
-  for(i = 0; image->zones[i]; i++) {
-    for(j = 0; image->zones[i]->segment[j]; j++) {
+  for (i = 0; image->zones[i]; i++) {
+    for (j = 0; image->zones[i]->segment[j]; j++) {
       seg = image->zones[i]->segment[j];
 
       invert_color(pix_at_img(seg->xd, seg->y));
--- src/algorithm.h
+++ src/algorithm.h
@@ -3,8 +3,12 @@
 
 #include "utilities.h"
 
+void generate_segment(Zone_t zone, Pixel_t pix);
 void generate_zone(Pixel_t pix);
-
 void make_zones();
+void change_color(Pixel_t pix, unsigned char R, unsigned char G,
+                  unsigned char B);
+void invert_color(Pixel_t pix);
+void black_border();
 
 #endif /* IMGTACHES_SRC_ALGORITHM_H_ */
